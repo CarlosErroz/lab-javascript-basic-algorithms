04255650ed19454223f6fa9cc921195ad3407f10
Carlos Erroz <carlos.erroz@hotmail.com>
2022-10-25 22:16:20 +0200

    Hecho

--- js/index.js
+++ js/index.js
@@ -1,6 +1,37 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+console.log("I'm ready!");
 
+// Iteration 1: Names and Input
+console.log ("Iteration 1: Names and Input:")
+let hacker1 = "Carlos";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Chrome";
+console.log(`The navigator's name is ${hacker2}`);
+console.log("\n")
+
+
+
+// Iteration 2: Conditionals
+console.log ("Iteration 2: Conditionals:")
+if (hacker1.length>hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+} else if (hacker1.length<hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}
+console.log("\n")
 
 // Iteration 3: Loops
+console.log ("Iteration 3: Loops:")
+let hacker1UpperCase = "";
+for (let i=0; i<hacker1.length; i++) {
+    hacker1UpperCase += hacker1[i].toUpperCase()
+    hacker1UpperCase += " "
+}
+console.log(hacker1UpperCase.slice(0, -1));
+let hacker2ReverseOrder = "";
+for (let i=hacker2.length; i>0; i--) {
+    hacker2ReverseOrder += hacker2[i-1];
+}
+console.log(hacker2ReverseOrder);
